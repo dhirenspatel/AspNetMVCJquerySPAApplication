ba505d0a1b32c2e460e07786f25ea5ea66d2d028
Dhiren Patel <patel>
2017-07-11 11:39:44 +0530

    Add project files.

---
+++ AspNetMVCCRUDJQuery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetMVCCRUDJQuery.MvcApplication" Language="C#" %>
---
+++ AspNetMVCCRUDJQuery/Models/EmployeeDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMVCCRUDJQuery.Models
+{
+    public class EmployeeDBContext: DbContext
+    {
+        public EmployeeDBContext() : base("DefaultConnection") { }
+        public DbSet<Employee> EmployeeDB { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
